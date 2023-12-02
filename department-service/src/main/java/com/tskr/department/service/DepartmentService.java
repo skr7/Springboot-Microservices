872dd6372a2bc9e953968f256ee9320684eadc73
Saikrishna <sairaju.jdev@gmail.com>
2023-12-02 14:06:00 +0530

    Added Service layer for Department class

--- department-service/src/main/java/com/tskr/department/service/DepartmentService.java
+++ department-service/src/main/java/com/tskr/department/service/DepartmentService.java
@@ -1,2 +1,26 @@
-package com.tskr.department.service;public class DepartmentService {
+package com.tskr.department.service;
+
+import com.tskr.department.entity.Department;
+import com.tskr.department.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@Slf4j
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department){
+        log.info("Department is Saved with name : {} ",department.getDepartmentName());
+        return departmentRepository.save(department);
+    }
+
+    public Department findDepartmentById(long departmentId){
+        log.info("fetching the department Id {}",departmentId);
+        return departmentRepository.findByDepartmentId(departmentId);
+    }
 }
